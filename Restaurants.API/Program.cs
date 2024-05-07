b56acaec9df5984ad243704a572e15397f3fdebc
Adam Eleraky <adam_eleraky@hotmail.com>
2024-05-07 20:15:37 +0200

    Connection string configured and DI for Infratructure abstracted away to the Infrastructure layer

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,8 +1,12 @@
+using Restaurants.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration); //Configuration gets all the info from appsettings.json thus connec string
 
 var app = builder.Build();
 
