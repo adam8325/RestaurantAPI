8ff25d054ff48b24fea43f0fd4a826d4ad096bd8
Adam Eleraky <adam_eleraky@hotmail.com>
2024-05-15 18:08:06 +0200

    FluentValidation added to CreateRestaurantDto

--- Restaurants.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
+++ Restaurants.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
@@ -5,4 +5,20 @@ namespace Restaurants.Application.Restaurants.Validators;
 
 public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
 {
+    private readonly List<string> validCategories = ["Arab", "Italian", "Somali"];
+    public CreateRestaurantDtoValidator()
+    {
+        RuleFor(dto => dto.Name)
+            .Length(3, 100);
+
+        RuleFor(dto => dto.Category)
+            .Must(validCategories.Contains)
+            .WithMessage("Please provide a valid category");
+        
+        RuleFor(dto => dto.ContactEmail)
+            .EmailAddress().WithMessage("Please provide a valid email address");
+
+        RuleFor(dto => dto.PostalCode)
+            .Matches(@"^\d{2}-\d{3}$").WithMessage("Please provide a valid postal code (xx-xxx)");
+    }
 }
