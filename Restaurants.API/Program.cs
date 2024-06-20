b6f6e670354e86909cb8106cb51f2611bdbfe392
Adam Eleraky <adam_eleraky@hotmail.com>
2024-06-20 11:50:29 +0200

    Moved Serilog configuration to the appsettings.JSON instead of Program.cs

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -2,6 +2,7 @@ using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
+using Serilog.Formatting.Compact;
 using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,13 +16,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration); //Configuration gets all the info from appsettings.json thus connec string
 builder.Services.AddApplication();
 builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration
-    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-    .WriteTo.File("Logs/Restaurant-API.log", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine} {Message:lj}{NewLine}{Exception}");
-});
+    configuration.ReadFrom.Configuration(context.Configuration)
+);
 
 var app = builder.Build();
 
