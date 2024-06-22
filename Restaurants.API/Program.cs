637a4c56b4331d966835cda8ffca52f68653bdaa
Adam Eleraky <adam_eleraky@hotmail.com>
2024-06-22 09:41:56 +0200

    Identity tables added to database and migration applied

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -5,6 +5,7 @@ using Serilog;
 using Serilog.Formatting.Compact;
 using Serilog.Events;
 using Restaurants.API.Middlewares;
+using Restaurants.Domain.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -42,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
 
 app.UseAuthorization();
 
