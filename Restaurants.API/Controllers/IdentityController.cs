cee7e13666d5a2fae8657633b7603b13d862c30c
Adam Eleraky <adam_eleraky@hotmail.com>
2024-06-22 12:08:05 +0200

    Assign user role endpoint implemented in the newly IdentityController

---
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Domain.Constants;
+
+namespace Restaurants.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("userRole")]
+        [Authorize(Roles = UserRoles.Admin)]
+        public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+        {
+            await mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
