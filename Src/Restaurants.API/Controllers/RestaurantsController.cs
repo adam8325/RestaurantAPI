7ea364123317ece2d15ad92c64080dde4af3a9db
Adam Eleraky <adam_eleraky@hotmail.com>
2024-07-08 11:52:28 +0200

    Application in dev environment runs BlobStorage locally through Azurite. Web Api hosted on Azure uploads logo to storage account hosted on Azure

--- Src/Restaurants.API/Controllers/RestaurantsController.cs
+++ Src/Restaurants.API/Controllers/RestaurantsController.cs
@@ -77,7 +77,7 @@ public class RestaurantsController(IMediator mediator) : ControllerBase
         var command = new UploadRestaurantLogoCommand()
         {
             RestaurantId = id,
-            FileName = file.FileName,
+            FileName = $"{id}-{file.FileName}",
             File = stream
         };
 
